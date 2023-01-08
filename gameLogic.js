5b7b50fc17bb9c152d02469cee2307060fa283b0
suchethaleonidou <suchethaleonidou>
2023-01-08 13:42:51 -0500

    gamelogic commit 2

--- gameLogic.js
+++ gameLogic.js
@@ -4,9 +4,13 @@ let computerSequence=[];
 let playerSequence=[];
 //Create the start button to inititate the game. It should be hidden once it's clicked.
 const startButton = document.querySelector('.js-start');
+const info = document.querySelector('.js-info');
 
 function startGame() {
   startButton.classList.add('hidden');
+  info.classList.remove('hidden');
+  info.textContent = 'Wait for the computer';
 }
 
 startButton.addEventListener('click', startGame);
+
